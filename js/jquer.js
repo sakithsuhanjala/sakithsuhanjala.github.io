80d1c95594d5c0236243ae2b75a2fdcff7561b2c
sakithsuhanjala <63744150+sakithsuhanjala@users.noreply.github.com>
2020-04-16 01:57:12 +0530

    Update jquer.js

--- js/jquer.js
+++ js/jquer.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.crew_area').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
@@ -15,4 +15,19 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-
+$('.event_area').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
